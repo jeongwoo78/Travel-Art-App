0fb86203a1bd806d1d43cbfcafcdf5100dcb5bc5
jeongwoo78 <poly0533@gmail.com>
2025-04-08 07:35:46 +0900

    Add files via upload
    Initial Upload of Travel-Art-App files

---
+++ Travel-Art- App/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TravelArtApp from '../components/TravelArtApp';
+
+const Home = () => {
+  return (
+    <div>
+      <TravelArtApp />
+    </div>
+  );
+};
+
+export default Home;
